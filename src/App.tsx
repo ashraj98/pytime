42ce215eae1c03f6df6889ae833895cfe59a4f40
Bryan Hagedorn <hagedorn.43@osu.edu>
2020-09-08 18:15:20 -0400

    Created a search bar on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
+export default function CustomizedInputBase() {
+  const classes = useStyles();
+
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+    <div className="intro">
+        <h1 className="logo">
+            <img className="pie" src="https://www.pngarts.com/files/5/Apple-Pie-PNG-Image-Transparent-Background.png"/> Pytime
+        </h1>
+
+        <h2>Discover new video games in seconds</h2>
+
+
+         <Paper component="form" className={classes.root}>
+            <InputBase
+                className={classes.input}
+                placeholder="Search games or tags"
+                inputProps={{ 'aria-label': 'Search games or tags' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+            </IconButton>
+            <Divider className={classes.divider} orientation="vertical" />
+        </Paper>
+    </div>
+
     </Router>
   );
 }
-
-export default App;
