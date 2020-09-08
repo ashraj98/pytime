6ff2b07026ea607c60b47536a5934f49000390ae
SKhatri1029 <sahil.v.khatri@gmail.com>
2020-09-08 17:43:35 -0400

    Created framework for recommendation page

---
+++ src/UI/Recommendations/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    myLittlePonyCover,
+    myLittlePonyHorse,
+    skyrimCover,
+    skyrimHorse,
+    witcherCover,
+    witcherHorse,
+    zeldaCover,
+    zeldaHorse
+} from '../GamePhotos'
+
+
+function Recommendations() {
+    return (
+        <div className="Recommendations">
+            
+        </div>
+    );
+}
+
+export default Recommendations;
