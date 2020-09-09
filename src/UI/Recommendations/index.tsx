09af57832564fc768c263c3f649a6d35955c488d
Justin Perry <justinperry2164@gmail.com>
2020-09-09 15:01:35 -0400

    Add the game pictures and title to each card for the demo

--- src/UI/Recommendations/index.tsx
+++ src/UI/Recommendations/index.tsx
@@ -13,24 +13,21 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-
-import {
-    myLittlePonyCover,
-    myLittlePonyHorse,
-    skyrimCover,
-    skyrimHorse,
-    witcherCover,
-    witcherHorse,
-    zeldaCover,
-    zeldaHorse
-} from '../GamePhotos'
+import skyrimCover from '../GamePhotos/skyrimCover.png';
+import skyrimHorse from '../GamePhotos/skyrimHorse.jpg';
+import myLittlePonyCover from '../GamePhotos/MyLittlePonyCover.jpg';
+import myLittlePonyHorse from '../GamePhotos/myLittlePonyHorse.jpg';
+import witcherCover from '../GamePhotos/witcherCover.jpg';
+import witcherHorse from '../GamePhotos/witcherHorse.jpg';
+import zeldaCover from '../GamePhotos/ZeldaCover.jpg';
+import zeldaHorse from '../GamePhotos/zeldaHorse.jpg';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        PyTime
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -70,7 +67,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4];
+
 
 export default function Album() {
   const classes = useStyles();
@@ -91,69 +89,121 @@ export default function Album() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
+              Game Recommendations
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+             You wanted games with horses? Here are some games with horses.
             </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
           </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+          <Grid container spacing={2}>
+              <Grid item key={1} xs={12} sm={6} md={3}>
                 <Card className={classes.card}>
                   <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="300"
+                    image= {skyrimCover}
+                    title="Skyrim"
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      Skyrim: Elder Scolls V
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the content.
+                      Action Packed!
                     </Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small" color="primary">
                       View
                     </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+               <Grid item key={2} xs={12} sm={6} md={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="300"
+                    image= {witcherCover}
+                    title="The Witcher 3"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      The Witcher 3: Wild Hunt
+                    </Typography>
+                    <Typography>
+                      YESSSSSSSS
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
                     <Button size="small" color="primary">
-                      Edit
+                      View
                     </Button>
                   </CardActions>
                 </Card>
               </Grid>
-            ))}
+               <Grid item key={3} xs={12} sm={6} md={3}>
+                <Card className={classes.card}>
+                    <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="300"
+                    image= {myLittlePonyCover}
+                    title="My Little Pony"
+                    />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      My Little Pony
+                    </Typography>
+                    <Typography>
+                      NEYYYYYY
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+               <Grid item key={4} xs={12} sm={6} md={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="300"
+                    image= {zeldaCover}
+                    title="Breath of the Wild"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      The Legend of Zelda: Breath of the Wild
+                    </Typography>
+                    <Typography>
+                      RIDEEEEEEE
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
           </Grid>
         </Container>
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Footer
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
+          Please give us an A :)
         </Typography>
         <Copyright />
       </footer>
