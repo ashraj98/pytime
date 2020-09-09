6119946cbb5c7bdefd3b4cf1489c664c847c60fe
Bryan Hagedorn <hagedorn.43@osu.edu>
2020-09-09 13:43:31 -0400

    working form/input field on search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-}));
-
-export default function CustomizedInputBase() {
-  const classes = useStyles();
-
-  return (
-    <Router>
-
-    <div className="intro">
-        <h1 className="logo">
-            <img className="pie" src="https://www.pngarts.com/files/5/Apple-Pie-PNG-Image-Transparent-Background.png"/> Pytime
-        </h1>
-
-        <h2>Discover new video games in seconds</h2>
+import SearchBar from "material-ui-search-bar";
 
 
-         <Paper component="form" className={classes.root}>
-            <InputBase
-                className={classes.input}
+export default class App extends React.Component<{}, {value: string}> {
+
+  handleClick = () => {
+    console.log('this is:', this);
+  }
+     render() {
+      return (
+        <Router>
+        <div className="intro">
+            <h1 className="logo">
+                <img className="pie" alt="pie" src="https://www.pngarts.com/files/5/Apple-Pie-PNG-Image-Transparent-Background.png"/> Pytime
+            </h1>
+            <h2>Discover new video games in seconds</h2>
+            <SearchBar
                 placeholder="Search games or tags"
-                inputProps={{ 'aria-label': 'Search games or tags' }}
+                value=""
+                onChange={(newValue) => this.setState({ value: newValue })}
+                onRequestSearch={() => this.handleClick()}
             />
-            <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                <SearchIcon />
-            </IconButton>
-            <Divider className={classes.divider} orientation="vertical" />
-        </Paper>
-    </div>
-
-    </Router>
-  );
+        </div>
+        </Router>
+      );
+    }
 }
