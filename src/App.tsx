39e5d973ba8dab362c8974f1381372bb909f39e1
Bryan Hagedorn <hagedorn.43@osu.edu>
2020-09-09 16:30:20 -0400

    Added routes to home + recommendations

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {Switch, Route, useHistory, Redirect} from 'react-router-dom';
 import './App.scss';
 import SearchBar from "material-ui-search-bar";
+import Recommendations from "./UI/Recommendations";
+import Home from "./UI/Home";
 
 
-export default class App extends React.Component<{}, {value: string}> {
+function App() {
 
-  handleClick = () => {
-    console.log('this is:', this);
-  }
-     render() {
       return (
         <Router>
-        <div className="intro">
-            <h1 className="logo">
-                <img className="pie" alt="pie" src="https://www.pngarts.com/files/5/Apple-Pie-PNG-Image-Transparent-Background.png"/> Pytime
-            </h1>
-            <h2>Discover new video games in seconds</h2>
-            <SearchBar
-                placeholder="Search games or tags"
-                value=""
-                onChange={(newValue) => this.setState({ value: newValue })}
-                onRequestSearch={() => this.handleClick()}
-            />
+        <div>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/home"/>}/>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/recommendations">
+              <Recommendations />
+            </Route>
+          </Switch>
+
         </div>
-        </Router>
+      </Router>
       );
-    }
 }
+
+export default App;
