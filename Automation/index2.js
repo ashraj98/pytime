040134f500f5c9c392b74a527fac6ca4e01f4954
negretenico <negretenico0@gmail.com>
2020-11-09 14:30:14 -0500

    MORE TESTS

--- Automation/index2.js
+++ Automation/index2.js
@@ -6,8 +6,48 @@ async function test1()
     let driver = new Builder().forBrowser("chrome").build();
     await driver.get("http://localhost:3000/login");
     await driver.findElement(By.name("username")).sendKeys("TEST-USERNAME",Key.RETURN);
-    console.log("TEST");
     await driver.findElement(By.name("password")).sendKeys("PAS$W0RD!",Key.RETURN);
 
 }
+async function test2()
+{
+    
+    let driver = new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/login");
+    await driver.findElement(By.name("username")).sendKeys("",Key.RETURN);
+    await driver.findElement(By.name("password")).sendKeys("PAS$W0RD!",Key.RETURN);
+
+}
+async function test3()
+{
+    
+    let driver = new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/login");
+    await driver.findElement(By.name("username")).sendKeys("US3R",Key.RETURN);
+    await driver.findElement(By.name("password")).sendKeys("",Key.RETURN);
+
+}
+async function test4()
+{
+    
+    let driver = new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/login");
+    await driver.findElement(By.name("username")).sendKeys("",Key.RETURN);
+    console.log("TEST");
+    await driver.findElement(By.name("password")).sendKeys("",Key.RETURN);
+
+}
+async function test5()
+{
+    
+    let driver = new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/login");
+    await driver.findElement(By.name("username")).sendKeys(null,Key.RETURN);
+    await driver.findElement(By.name("password")).sendKeys("DATA",Key.RETURN);
+
+}
 test1();
+test2();
+test3();
+test4();
+test5();
